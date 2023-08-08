63097adfb2bf5c1e7746f2fd8efb4620d93a0350
Carla Villagra <villagraconce06@gmail.com>
2023-08-08 14:58:57 -0300

    primera entrega

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 
-
 let usuario = "carla1";
 let pin = "12345";
 let ingresar = false;
@@ -10,63 +9,52 @@ function ingreso() {
         let ingresoPIN = prompt("Ingresa tu PIN");
 
         if (ingresoUSER == usuario && ingresoPIN == pin) {
-
-            alert(" Bienvenida " + usuario);
+            alert("Bienvenida " + usuario);
             ingresar = true;
             break;
         } else {
-            alert("Error este usuario no existe")
-            
+            alert("Error este usuario no existe");
         }
     }
-
 }
-ingreso()
 
-if (ingresar) {
-    let productos = prompt("Elegi un producto: \n1- Campera argentina. \n2- Pantalon de boca. \n3- Zapatillas nike. ");
+function realizarCompra() {
+    let productos = prompt(
+        "Elegi un producto: \n1- Campera argentina. \n2- Pantalon de boca. \n3- Zapatillas nike. "
+    );
 
-        switch (productos) {
+    switch (productos) {
+        case "1":
+            let campera = 7500;
+            alert("El precio de campera argentina es " + campera);
+            plandePago(7500, " Campera argentina ");
+            break;
 
-            case "1":
-                let campera = 7500;
-                alert("El precio de campera argentina es " + campera);
-                plandePago( 7500, " Campera argentina ");
+        case "2":
+            let pantalon = 5000;
+            alert("El precio del pantalon de boca es " + pantalon);
+            plandePago(5000, " Pantalon de boca ");
+            break;
 
-                break;
+        case "3":
+            let zapatilla = 9500;
+            alert("El precio de las zapatillas es " + zapatilla);
+            plandePago(9500, " Zapatillas nike ");
+            break;
 
-            case "2":
-                let pantalon = 5000;
-                alert("El precio del pantalon de boca es " + pantalon);
-                plandePago( 5000, " Pantalon de boca ");
-
-                break;
-
-            case "3":
-                let zapatilla = 9500;
-                alert("El precio de las zapatillas es " + zapatilla);
-                plandePago( 9500, " Zapatillas nike ")
-
-                break;
-
-            default:
-                alert("Opcion invalida");
-                break;
-
-        }
-        
-        productos = prompt(
-            "Elegí un producto: \n1-Campera argentina.  \n2 - Pantalon de boca. \n3 - Zapatillas nike. ");
-
-    
-    
+        default:
+            alert("Opcion invalida");
+            break;
+    }
+}
 
 function plandePago(precio, producto) {
     let total;
-    const formadePago = prompt("Como quiere pagar: \nEfectivo \nTranferencia \nDebito \nCredito ");
+    const formadePago = prompt(
+        "Como quiere pagar: \nEfectivo \nTransferencia \nDebito \nCredito "
+    );
 
     switch (formadePago) {
-
         case "efectivo":
             total = precio;
             alert(`El total de ${producto}, en efectivo es de $${total}`);
@@ -74,9 +62,8 @@ function plandePago(precio, producto) {
 
         case "transferencia":
             total = precio - 200;
-            alert(`El total de ${producto}, por tranferencia es de $${total}`);
+            alert(`El total de ${producto}, por transferencia es de $${total}`);
             break;
-
 
         case "debito":
             total = precio * 1.10;
@@ -89,28 +76,29 @@ function plandePago(precio, producto) {
             if (cuotas == 6) {
                 total = precio / 6 + 2500;
                 alert(`El total de ${producto}, en 6 cuotas es de $${total}`);
-
-            }
-            else {
+            } else {
                 total = precio / 12 + 3500;
                 alert(`El total de ${producto}, en 12 cuotas es de $${total}`);
             }
-
             break;
-
-
     }
-    alert(" Gracias por tu compra " + usuario);
+
     
+    if (confirm("Gracias por tu compra " + usuario + ". ¿Deseas realizar otra compra?")) {
+        realizarCompra();
+    } else {
+        
+        alert("Gracias por tu compra. ¡Hasta pronto!");
+    }
 }
 
-}
-else{
+ingreso();
+
+if (ingresar) {
+    realizarCompra();
+} else {
     alert("Cuenta suspendida por 5 dias.");
-    
 }
-
-
 
 
 
